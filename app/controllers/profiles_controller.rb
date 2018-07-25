6af3c19a4aeccba1857b8437c4d1fc0295074c0e
Shadi Abumattar <shadiabumattar@live.com>
2018-07-25 09:24:52 +0000

    Added profile form view, controller action and route

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,10 @@
+class ProfilesController < ApplicationController
+  # GET request to /users/:user_id/profile/new
+  def new
+    # Render blank profile form
+  end
+  
+  def create
+    
+  end
+end
