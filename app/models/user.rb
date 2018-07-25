e528eed7d06e970f6694d697ecd89dfaf551479c
Shadi Abumattar <shadiabumattar@live.com>
2018-07-25 08:13:57 +0000

    Added an association betweeen profiles and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
+  has_one :profile
   
   attr_accessor :stripe_card_token
   # If a pro user passes validations, call Stripe and let Stripe set up a
