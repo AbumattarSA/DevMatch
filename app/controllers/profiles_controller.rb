05ad3f789680f531da7592bb6ed33605c2a743b8
Shadi Abumattar <shadiabumattar@live.com>
2018-07-26 08:29:48 +0000

    Added profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,7 @@ class ProfilesController < ApplicationController
   # GET request to /users/:user_id/profile/new
   def new
     # Render blank profile form
+    @profile = Profile.new
   end
   
   def create
