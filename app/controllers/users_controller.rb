75e0fbf536f0e7cd9de382219575dab266fc8e4b
Shadi Abumattar <shadiabumattar@live.com>
2018-07-29 11:53:39 +0000

    Added before filters to check user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   
+  before_action :authenticate_user!
+  
   # GET request to /users/:id
   def show
-    @user = User.find (params[:id])
+    @user = User.find( params[:id] )
   end
   
 end
